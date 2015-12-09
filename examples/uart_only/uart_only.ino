ec1f627005b53da8cbdb7e1b6a92416111e535be
extrapixel <patrick@whiteboxes.ch>
2015-12-09 02:15:45 +0100

    comments & formatting

--- examples/uart_only/uart_only.ino
+++ examples/uart_only/uart_only.ino
@@ -1,10 +1,13 @@
 // WhiteBox Labs -- Tentacle Shield --  UART example
+// www.whiteboxes.ch
 //
-// Simple example on how to use 4 (or 8 if using 2 Tentacle shields) Atlas Scientivic devices
-// in serial mode. This sketch assumes all of your devices are either older serial devices or
-// EZO circuits in serial mode. You can use the tentacle_setup.ino to autodetect and setup your devices.
+// This code is based on https://www.atlas-scientific.com/_files/code/4-port-board.pdf
 //
-// This code is intended to work on all Arduinos. If using the Arduino Yun, connect
+// How to use 4 (or 8 if using 2 Tentacle shields) Atlas Scientivic devices in serial mode.
+// This sketch assumes all of your devices are either older serial devices or EZO circuits
+// in serial mode. You can use the tentacle_setup.ino to autodetect and setup your devices.
+//
+// This code is intended to work on all 5V-tolerant Arduinos. If using the Arduino Yun, connect
 // to it's serial port. If you want to work with the Yun wirelessly, check out the respective
 // Yun version of this example.
 //
@@ -39,79 +42,73 @@
 
 #include <SoftwareSerial.h>         //Include the software serial library  
 
-SoftwareSerial sSerial(11, 10);    // RX, TX  - Name the software serial library sftSerial (this cannot be omitted)
+SoftwareSerial sSerial(11, 10);     // RX, TX  - Name the software serial library sftSerial (this cannot be omitted)
                                     // assigned to pins 10 and 11 for maximum compatibility
 int s0 = 7;                         // Tentacle uses pin 7 for multiplexer control S0
 int s1 = 6;                         // Tentacle uses pin 6 for multiplexer control S1
 int enable_1 = 5;	            // Tentacle uses pin 5 to control pin E on shield 1
 int enable_2 = 4;	            // Tentacle uses pin 4 to control pin E on shield 2
 
-char computerdata[20];              //A 20 byte character array to hold incoming data from a pc/mac/other
-char sensordata[30];                //A 30 byte character array to hold incoming data from the sensors
-byte computer_bytes_received = 0;   //We need to know how many characters bytes have been received
-byte sensor_bytes_received = 0;     //We need to know how many characters bytes have been received
+char computerdata[20];              // A 20 byte character array to hold incoming data from a pc/mac/other
+char sensordata[30];                // A 30 byte character array to hold incoming data from the sensors
+byte computer_bytes_received = 0;   // We need to know how many characters bytes have been received
+byte sensor_bytes_received = 0;     // We need to know how many characters bytes have been received
 
-char *channel;                      //Char pointer used in string parsing
-char *cmd;                          //Char pointer used in string parsing
+char *channel;                      // Char pointer used in string parsing
+char *cmd;                          // Char pointer used in string parsing
 
 
 
 void setup() {
-  pinMode(s0, OUTPUT);             //Set the digital pin as output.
-  pinMode(s1, OUTPUT);             //Set the digital pin as output.
-  pinMode(enable_1, OUTPUT);       //Set the digital pin as output.
-  pinMode(enable_2, OUTPUT);       //Set the digital pin as output.
+  pinMode(s0, OUTPUT);             // set the digital output pins for the serial multiplexer
+  pinMode(s1, OUTPUT);
+  pinMode(enable_1, OUTPUT);
+  pinMode(enable_2, OUTPUT);
 
-  Serial.begin(9600);              //Set the hardware serial port to 9600
-  sSerial.begin(9600);             //Set the soft serial port to 9600 (change if all your devices use another baudrate)
+  Serial.begin(9600);              // Set the hardware serial port to 9600
+  sSerial.begin(9600);             // Set the soft serial port to 9600 (change if all your devices use another baudrate)
   intro();                         // display startup message
 }
 
 
-void serialEvent() {              //This interrupt will trigger when the data coming from the serial monitor(pc/mac/other) is received
+void serialEvent() {               //This interrupt will trigger when the data coming from the serial monitor(pc/mac/other) is received
   computer_bytes_received = Serial.readBytesUntil(13, computerdata, 20); //We read the data sent from the serial monitor(pc/mac/other) until we see a <CR>. We also count how many characters have been received
-  computerdata[computer_bytes_received] = 0; //We add a 0 to the spot in the array just after the last character we received.. This will stop us from transmitting incorrect data that may have been left in the buffer
-}
-
-
-void intro() {                                  // print intro
-  Serial.flush();
-  Serial.println(" ");
-  Serial.println("READY_");
+  computerdata[computer_bytes_received] = 0;      //We add a 0 to the spot in the array just after the last character we received.. This will stop us from transmitting incorrect data that may have been left in the buffer
 }
 
 
 void loop() {
 
-  if (computer_bytes_received != 0) {             //If computer_bytes_received does not equal zero
-    channel = strtok(computerdata, ":");          //Let's parse the string at each colon
-    cmd = strtok(NULL, ":");                      //Let's parse the string at each colon
-    open_channel();                               //Call the function "open_channel" to open the correct data path
-    if (cmd != 0) {                               //if no command has been sent, send nothing
-      sSerial.print(cmd);                        //Send the command from the computer to the Atlas Scientific device using the softserial port
-      sSerial.print("\r");                       //After we send the command we send a carriage return <CR>
+  if (computer_bytes_received != 0) {             // If computer_bytes_received does not equal zero
+    channel = strtok(computerdata, ":");          // Let's parse the string at each colon
+    cmd = strtok(NULL, ":");
+    open_channel();                               // Call the function "open_channel" to open the correct data path
+    if (cmd != 0) {                               // if no command has been sent, send nothing
+      sSerial.print(cmd);                         // Send the command from the computer to the Atlas Scientific device using the softserial port
+      sSerial.print("\r");                        // <CR> carriage return to terminate message
     }
-    computer_bytes_received = 0;                  //Reset the var computer_bytes_received to equal 0
+    computer_bytes_received = 0;                  // Reset the var computer_bytes_received
   }
 
-  if (sSerial.available() > 0) {                 //If data has been transmitted from an Atlas Scientific device
+  if (sSerial.available() > 0) {                 // If data has been transmitted from an Atlas Scientific device
     sensor_bytes_received = sSerial.readBytesUntil(13, sensordata, 30); //we read the data sent from the Atlas Scientific device until we see a <CR>. We also count how many character have been received
-    sensordata[sensor_bytes_received] = 0;         //we add a 0 to the spot in the array just after the last character we received. This will stop us from transmitting incorrect data that may have been left in the buffer
-    Serial.println(sensordata);                    //let’s transmit the data received from the Atlas Scientific device to the serial monitor
+    sensordata[sensor_bytes_received] = 0;       // we add a 0 to the spot in the array just after the last character we received. This will stop us from transmitting incorrect data that may have been left in the buffer
+    Serial.println(sensordata);                  // let’s transmit the data received from the Atlas Scientific device to the serial monitor
   }
 }
 
 
-void open_channel() {                        //This function controls what UART port is opened.
+// Open a channel via the Tentacle serial multiplexer
+void open_channel() {
 
-  switch (*channel) {                        //Looking to see what channel to open
+  switch (*channel) {
 
-    case '0':                                //If channel==0 then we open channel 0
-      digitalWrite(enable_1, LOW);           //Setting enable_1 to low activates primary channels: 0,1,2,3
-      digitalWrite(enable_2, HIGH);          //Setting enable_2 to high deactivates secondary channels: 4,5,6,7
-      digitalWrite(s0, LOW);                 //S0 and S1 control what channel opens
-      digitalWrite(s1, LOW);                 //S0 and S1 control what channel opens
-      break;                                 //Exit switch case
+    case '0':                                // if channel==0 then we open channel 0
+      digitalWrite(enable_1, LOW);           // setting enable_1 to low activates primary channels: 0,1,2,3
+      digitalWrite(enable_2, HIGH);          // setting enable_2 to high deactivates secondary channels: 4,5,6,7
+      digitalWrite(s0, LOW);                 // S0 and S1 control what channel opens
+      digitalWrite(s1, LOW);                 // S0 and S1 control what channel opens
+      break;
 
     case '1':
       digitalWrite(enable_1, LOW);
@@ -162,4 +159,12 @@ void open_channel() {                        //This function controls what UART 
       digitalWrite(s1, HIGH);
       break;
   }
+}
+
+
+// Print intro
+void intro() {
+  Serial.flush();
+  Serial.println(" ");
+  Serial.println("READY_");
 }
--- examples/yun_uart_only/yun_uart_only.ino
+++ examples/yun_uart_only/yun_uart_only.ino
@@ -1,8 +1,11 @@
-// WhiteBox Labs -- Tentacle Shield --  UART example
+// WhiteBox Labs -- Tentacle Shield & Arduino Yun --  UART example
+// www.whiteboxes.ch
 //
-// Simple example on how to use 4 (or 8 if using 2 Tentacle shields) Atlas Scientivic devices
-// in serial mode. This sketch assumes all of your devices are either older serial devices or
-// EZO circuits in serial mode. You can use the tentacle_setup.ino to autodetect and setup your devices.
+// This code is based on https://www.atlas-scientific.com/_files/code/4-port-board.pdf
+//
+// How to use 4 (or 8 if using 2 Tentacle shields) Atlas Scientivic devices in serial mode.
+// This sketch assumes all of your devices are either older serial devices or EZO circuits
+// in serial mode. You can use the tentacle_setup.ino to autodetect and setup your devices.
 //
 // This sample code was written on an Arduino YUN, and depends on it's Bridge library to
 // communicate wirelessly.
@@ -16,7 +19,7 @@
 // - Serial channel numbers are 0-3
 //   ( Channels 4-7 are also available, if you're using two stacked Tentacle shields)
 //
-// - To open a channel send the number of the channel, a colon and the command ending with a carriage return.
+// - To open a channel, send the number of the channel, a colon and the command ending with a carriage return.
 //
 // 0:r<CR>
 // 1:i<CR>
@@ -40,40 +43,40 @@
 //
 //---------------------------------------------------------------------------------------------
 
-#include <SoftwareSerial.h>         //Include the software serial library
+#include <SoftwareSerial.h>         // software serial library
 #include <Console.h>                // Yun Console
 
-SoftwareSerial sSerial(11, 10);    // RX, TX  - Name the software serial library sftSerial (this cannot be omitted)
+SoftwareSerial sSerial(11, 10);     // RX, TX  - Name the software serial library sftSerial (this cannot be omitted)
                                     // assigned to pins 10 and 11 for maximum compatibility
 int s0 = 7;                         // Tentacle uses pin 7 for multiplexer control S0
 int s1 = 6;                         // Tentacle uses pin 6 for multiplexer control S1
 int enable_1 = 5;	            // Tentacle uses pin 5 to control pin E on shield 1
 int enable_2 = 4;	            // Tentacle uses pin 4 to control pin E on shield 2
 
-char computerdata[20];              //A 20 byte character array to hold incoming data from a pc/mac/other
+char computerdata[20];              // A 20 byte character array to hold incoming data from a pc/mac/other
 byte computer_bytes_received = 0;   // We need to know how many characters bytes have been received
 int computer_in_byte;               // a variable to read incoming console data into
 boolean computer_msg_complete = false;
 
-char sensordata[30];                //A 30 byte character array to hold incoming data from the sensors
-byte sensor_bytes_received = 0;     //We need to know how many characters bytes have been received
+char sensordata[30];                // a 30 byte character array to hold incoming data from the sensors
+byte sensor_bytes_received = 0;     // we need to know how many characters bytes have been received
 
-char *channel;                      //Char pointer used in string parsing
-char *cmd;                          //Char pointer used in string parsing
+char *channel;                      // char pointer used in string parsing
+char *cmd;                          // char pointer used in string parsing
 
 
 
 void setup() {
-  pinMode(s0, OUTPUT);             //Set the digital pin as output.
-  pinMode(s1, OUTPUT);             //Set the digital pin as output.
-  pinMode(enable_1, OUTPUT);       //Set the digital pin as output.
-  pinMode(enable_2, OUTPUT);       //Set the digital pin as output.
+  pinMode(s0, OUTPUT);             // set the digital output pins for the serial multiplexer
+  pinMode(s1, OUTPUT);
+  pinMode(enable_1, OUTPUT);
+  pinMode(enable_2, OUTPUT);
 
   Bridge.begin();
-  Console.begin();                  // initialize serial communication over network:
-  while (!Console) ;                // wait for Console port to connect.
-  sSerial.begin(9600);              //Set the soft serial port to 9600 (change if all your devices use another baudrate)
-  intro();                          // display startup message
+  Console.begin();                 // initialize serial communication over network:
+  while (!Console) ;               // wait for Console port to connect.
+  sSerial.begin(9600);             // set the soft serial port to 9600
+  intro();                         // display startup message
 }
 
 
@@ -86,48 +89,42 @@ void loop() {
       computerdata[computer_bytes_received] = 0;
       computer_msg_complete = true;
       computer_bytes_received = 0;
-    } else {                                     // or just ad the byte to 
+    } else {                                    // add the new byte to the computerdata string
       computerdata[computer_bytes_received] = computer_in_byte;
       computer_bytes_received++;
     }
   }
 
-  if (computer_msg_complete) {                  // If we received a command from the computer
-    channel = strtok(computerdata, ":");          //Let's parse the string at each colon
-    cmd = strtok(NULL, ":");                      //Let's parse the string at each colon
-    open_channel();                               //Call the function "open_channel" to open the correct data path
-    if (cmd != 0) {                               //if no command has been sent, send nothing
-      sSerial.print(cmd);                        //Send the command from the computer to the Atlas Scientific device using the softserial port
-      sSerial.print("\r");                       //After we send the command we send a carriage return <CR>
+  if (computer_msg_complete) {                  // if we received a command from the computer
+    channel = strtok(computerdata, ":");        // let's parse the string at each colon
+    cmd = strtok(NULL, ":");                    // let's parse the string at each colon
+    open_channel();                             // call the function "open_channel" to open the correct multiplexed serial channel
+    if (cmd != 0) {                             // if no command has been sent, send nothing
+      sSerial.print(cmd);                       // send the command from the computer to the Atlas Scientific device using the softserial port
+      sSerial.print("\r");                      // after we send the command we send a carriage return <CR>
     }
-    computer_msg_complete = false;              //Reset the var computer_msg_complete to be ready for the next command
+    computer_msg_complete = false;              // reset the var computer_msg_complete to be ready for the next command
   }
 
-  if (sSerial.available() > 0) {                 //If data has been transmitted from an Atlas Scientific device
+  if (sSerial.available() > 0) {                 // if data has been transmitted from an Atlas Scientific device
     sensor_bytes_received = sSerial.readBytesUntil(13, sensordata, 30); //we read the data sent from the Atlas Scientific device until we see a <CR>. We also count how many character have been received
-    sensordata[sensor_bytes_received] = 0;         //we add a 0 to the spot in the array just after the last character we received. This will stop us from transmitting incorrect data that may have been left in the buffer
-    Console.println(sensordata);                    //let’s transmit the data received from the Atlas Scientific device to the serial monitor
+    sensordata[sensor_bytes_received] = 0;       // we add a 0 to the spot in the array just after the last character we received. This will stop us from transmitting incorrect data that may have been left in the buffer
+    Console.println(sensordata);                 // let’s transmit the data received from the Atlas Scientific device to the serial monitor
   }
 }
 
 
-void intro() {                                  // print intro
-  Console.flush();
-  Console.println(" ");
-  Console.println("READY_");
-}
+// Open a channel via the Tentacle serial multiplexer
+void open_channel() {
 
+  switch (*channel) {
 
-void open_channel() {                        //This function controls what UART port is opened.
-
-  switch (*channel) {                        //Looking to see what channel to open
-
-    case '0':                                //If channel==0 then we open channel 0
-      digitalWrite(enable_1, LOW);           //Setting enable_1 to low activates primary channels: 0,1,2,3
-      digitalWrite(enable_2, HIGH);          //Setting enable_2 to high deactivates secondary channels: 4,5,6,7
-      digitalWrite(s0, LOW);                 //S0 and S1 control what channel opens
-      digitalWrite(s1, LOW);                 //S0 and S1 control what channel opens
-      break;                                 //Exit switch case
+    case '0':                                // if channel==0 then we open channel 0
+      digitalWrite(enable_1, LOW);           // setting enable_1 to low activates primary channels: 0,1,2,3
+      digitalWrite(enable_2, HIGH);          // setting enable_2 to high deactivates secondary channels: 4,5,6,7
+      digitalWrite(s0, LOW);                 // S0 and S1 control what channel opens
+      digitalWrite(s1, LOW);                 // S0 and S1 control what channel opens
+      break;
 
     case '1':
       digitalWrite(enable_1, LOW);
@@ -178,4 +175,12 @@ void open_channel() {                        //This function controls what UART 
       digitalWrite(s1, HIGH);
       break;
   }
+}
+
+
+// Print intro
+void intro() {
+  Console.flush();
+  Console.println(" ");
+  Console.println("READY_");
 }
